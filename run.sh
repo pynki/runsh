d89d458c347e58e086816def30a8441ae831d7ff
brain <brain@42specifications.com>
2017-08-17 07:04:59 +0800

    added check for installed packages to tools

--- run.sh
+++ run.sh
@@ -15,9 +15,12 @@ function quit {
 # SOURCING OF OTHER SCRIPTS
 ################################################################################
 #TODO this needs to be done in a function
-source tools/logger.conf.sh
+source conf/logger.conf
 source tools/logger.sh
 source tools/tools.sh
+
+### TODO: jq config etc
+source conf/runsh.conf
 
 ################################################################################
 # DIRECTORY AND SCRIPTNAME
@@ -37,17 +40,13 @@ log "DIR is $DIR" 0
 
 ### check if given config is GOOD
 if [ -z "$1" ]; then
-	quit '$1 not given - cannot execute "nothing"'
+	quit '$1 not given - cannot source "nothing"'
 else
 	if [ ! -f "$1" ]; then
         quit "File $1 does not exist!" 1
 	else
-		if [ ! -x "$1" ]; then
-            quit "File $1 is not executable, try 'chmod +x $1' to make it executable." 1
-		else
 		log "Sourceing $1" 1
 		source $1
-		fi
 	fi
 fi
 ### see if the given script is GOOD
--- tools/tools.sh
+++ tools/tools.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #defines how often the online checker pings the target
-TOOLS_CHECK_ONLINE_COUNT=3
+
 
 # Checks if a host is online
 # It just tried to ping it serveral times and see if the host answers
 #call: checkOnline @TARGET_IP
 checkOnline() {
+	local TOOLS_CHECK_ONLINE_COUNT=3 #TODO: rethink and rename...
 	if [ "$#" -ne 2 ]; then
 		log "Check if host [$1] is online" 0
 		ping -q -c$TOOLS_CHECK_ONLINE_COUNT $1 > /dev/null
@@ -28,7 +29,7 @@ checkOnline() {
 netCopy() {
 	if [ "$#" -ne 6 ]; then	
 		log "netCopy() - Copy $1 to $4@$3:$2" 1
-		sshpass -p $5 | scp -q $1 $4@$3:$2
+		`sshpass -p $5 scp -q $1 $4@$3:$2`
 	else
 		log "netCopy() - params wrong [$*]" 2
 		return 1
@@ -54,10 +55,76 @@ netExe() {
 		log "netExe() - params wrong [$*]" 2
 		return 1
 	fi
-	log "Return of netExe is: $SSH_RET" 1
+	log "Output of netExe is: $SSH_RET" 1
+	return 0
+}
+
+# Checks if a host is configured in a ssh config file
+# call: checkHostExistsInSSHConfig $SSH_HOSTNAME
+# needs $SSH_CONFIG configutred and pointing to an ssh config file
+checkHostExistsInSSHConfig() {
+	if [ -z "$SSH_CONFIG" ]; then
+		log "No SSH_CONFIG param present." 2
+		return 1
+	else
+		if [ ! -f "$SSH_CONFIG" ]; then
+			log "Missing $SSH_CONFIG" 2
+			return 1
+		else
+		# assumes that at this point we have a valid ssh config file (TODO: check if its really valid!)
+			GREP_RET=`cat $SSH_CONFIG | grep -x "Host $1"`
+			if [ ! $? -eq 0 ]; then
+				log "Cannot find host $1 in ssh config file: $SSH_CONFIG" 1
+				return 1
+			else
+				log "Found host $1 in $SSH_CONFIG" 0
+			fi
+		fi
+	fi
+	return 0
+}
+
+# Checks if a packet is installed
+# call: checkIfInstalled REMOTE PACKAGE_NAME INSTALL_IF_NOT_INSTALLED(any input)
+# needs dpkg
+#TODO nicer output....
+checkIfInstalled() {
+	local DPKG_L_NOT_FOUND="no"
+	if [ $1 -eq 1 ]; then
+		echo "exe remote"
+		#TODO this must somehow get the user-host config....
+		NETEXE_RET=`netExe 0 "dpkg-query -W $2 2>&1" 10.254.0.22 root 1234`
+		grep -q "$DPKG_L_NOT_FOUND" <<< $NETEXE_RET
+	else
+		echo "exe local"
+		dpkg-query -W $2 2>&1 | grep "$DPKG_L_NOT_FOUND"
+		fi	
+	if [ $? -eq 0 ]; then
+		log "Packet $2 is not installed" 1
+		if [ $3 -eq 0 ]; then
+			return 1
+		else
+			log "Try to install $2 with [installWithApt $3 $2]" 1
+			installWithApt 1 $2
+		fi	
+	else
+		log "Packet $2 is installed." 0
+	fi
+	return 0
+}
+
+# Installs packages
+# call: 
+# needs apt-get
+installWithApt() {
+	echo "updateing apt lists because 1"
+	echo "installing $2"
 	return 0
 }
 
 export -f netExe
 export -f netCopy
 export -f checkOnline
+export -f checkHostExistsInSSHConfig
+export -f checkIfInstalled
+export -f installWithApt
